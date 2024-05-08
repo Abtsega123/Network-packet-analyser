148fbaa3cf65be16be017e3f4dba96db3171655e
Abtsega123 <andyboringo1@gmail.com>
2024-05-08 11:21:26 +0530

    commited

--- sniffer_demo.py
+++ sniffer_demo.py
@@ -8,10 +8,10 @@ def main():
 
 
     while True:
-        raw_data, addr = conn_recvfrom(65536)
+        raw_data, addr = conn.recvfrom(65536)
         dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
         print('\nEthernet Frame:')
-        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
+        print( TAB_1 + 'Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
 
         
 #unpack ethernet frame
@@ -24,5 +24,34 @@ def get_mac_addr(bytes_addr):
     bytes_str = map('{:02X}'.format, bytes_addr)
     return ':'.join(bytes_str).upper()
 
+
+#unpack ipv4 frame
+def ipv4_frame(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:] 
+
+#returns properly formatted ipv4 address
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
+
+#unpack icmp packet
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgment, ofset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    falg_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgment, flag_urg, falg_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:], ofset_reserved_flags
 
 main()
